986499147b05ab8c580ed16ede2694b4cd3e6d34
Ashley Mannix <ashleymannix@live.com.au>
2015-08-27 20:27:57 +1000

    Implementing Process as IProcessStep to allow executing sub processes

--- src/cirrus.core.workflow/processes/Process.cs
+++ src/cirrus.core.workflow/processes/Process.cs
@@ -6,7 +6,9 @@ namespace Cirrus.Core.Workflow.Processes
 	//TODO: Investigate ways of automating the loop through processes
 	//This code is currently very manual and clunky, but if it remains
 	//fairly untouched 'framework' code then we can probably get away with it
-	public class Process<TIn, TOut> : IProcess
+
+	//TODO: Standardise repeat logic (error checks etc)
+	public class Process<TIn, TOut> : IProcess, IProcessStep<TIn, TOut>
 		where TIn : class, IProcessInput
 		where TOut : class, IProcessOutput
 	{
@@ -15,10 +17,21 @@ namespace Cirrus.Core.Workflow.Processes
 			_arg = arg;
 		}
 
+		public Process(IProcessStep<TIn, TOut> step)
+		{
+			_step = step;
+		}
+
 		private TIn _arg;
+		private IProcessStep<TIn, TOut> _step;
 
 		public async Task<TOut> ExecuteStep(IProcessStep<TIn, TOut> process)
 		{
+			if (_arg == null)
+			{
+				throw new NullReferenceException(string.Format("You must provide an argument of type {0} in order to execute with supplied IProcessStep", nameof(TIn))); 
+			}
+
 			return await process.Execute(_arg);
 		}
 
@@ -26,9 +39,21 @@ namespace Cirrus.Core.Workflow.Processes
 		{
 			return await ExecuteStep(process);
 		}
+
+		public async Task<TOut> Execute(TIn arg)
+		{
+			_arg = arg;
+
+			if (_step == null)
+			{
+				throw new NullReferenceException("You must provide an IProcessStep in order to execute with supplied argument");
+			}
+
+			return await Execute(_step);
+		}
 	}
 
-	public class Process<TIn1, TOut1, TIn2, TOut2> : IProcess
+	public class Process<TIn1, TOut1, TIn2, TOut2> : IProcess, IProcessStep<TIn1, TOut2>
 		where TIn1 : class, IProcessInput
 		where TOut1 : class, IProcessOutput
 		where TIn2 : class, IProcessInput
@@ -40,12 +65,27 @@ namespace Cirrus.Core.Workflow.Processes
 			_thread1 = thread1;
 		}
 
+		public Process(IProcessStep<TIn1, TOut1> step1, IProcessStep<TIn2, TOut2> step2, Func<TOut1, TIn2> thread1)
+			: this(null, thread1)
+		{
+			_step1 = step1;
+			_step2 = step2;
+		}
+
 		private TIn1 _arg;
+
+		private IProcessStep<TIn1, TOut1> _step1; 
+		private IProcessStep<TIn2, TOut2> _step2;
 
 		private Func<TOut1, TIn2> _thread1;
 
 		public async Task<Process<TIn2, TOut2>> ExecuteStep(IProcessStep<TIn1, TOut1> process)
 		{
+			if (_arg == null)
+			{
+				throw new NullReferenceException(string.Format("You must provide an argument of type {0} in order to execute with supplied IProcessStep", nameof(TIn1))); 
+			}
+
 			var result = await process.Execute(_arg);
 
 			return new Process<TIn2, TOut2>(_thread1(result));
@@ -57,9 +97,21 @@ namespace Cirrus.Core.Workflow.Processes
 
 			return await process2.Execute(_thread1(result));
 		}
+
+		public async Task<TOut2> Execute(TIn1 arg)
+		{
+			_arg = arg;
+
+			if (_step1 == null || _step2 == null)
+			{
+				throw new NullReferenceException("You must provide an IProcessStep in order to execute with supplied argument");
+			}
+
+			return await Execute(_step1, _step2);
+		}
 	}
 
-	public class Process<TIn1, TOut1, TIn2, TOut2, TIn3, TOut3> : IProcess
+	public class Process<TIn1, TOut1, TIn2, TOut2, TIn3, TOut3> : IProcess, IProcessStep<TIn1, TOut3>
 		where TIn1 : class, IProcessInput
 		where TOut1 : class, IProcessOutput
 		where TIn2 : class, IProcessInput
@@ -75,13 +127,30 @@ namespace Cirrus.Core.Workflow.Processes
 			_thread2 = thread2;
 		}
 
+		public Process(IProcessStep<TIn1, TOut1> step1, IProcessStep<TIn2, TOut2> step2, IProcessStep<TIn3, TOut3> step3, Func<TOut1, TIn2> thread1, Func<TOut2, TIn3> thread2)
+			: this(null, thread1, thread2)
+		{
+			_step1 = step1;
+			_step2 = step2;
+			_step3 = step3;
+		}
+
 		private TIn1 _arg;
+
+		private IProcessStep<TIn1, TOut1> _step1; 
+		private IProcessStep<TIn2, TOut2> _step2;
+		private IProcessStep<TIn3, TOut3> _step3;
 
 		private Func<TOut1, TIn2> _thread1;
 		private Func<TOut2, TIn3> _thread2;
 
 		public async Task<Process<TIn2, TOut2, TIn3, TOut3>> ExecuteStep(IProcessStep<TIn1, TOut1> process)
 		{
+			if (_arg == null)
+			{
+				throw new NullReferenceException(string.Format("You must provide an argument of type {0} in order to execute with supplied IProcessStep", nameof(TIn1))); 
+			}
+
 			var result = await process.Execute(_arg);
 
 			return new Process<TIn2, TOut2, TIn3, TOut3>(_thread1(result), _thread2);
@@ -93,6 +162,18 @@ namespace Cirrus.Core.Workflow.Processes
 			var result2 = await process2.Execute(_thread1(result1));
 
 			return await process3.Execute(_thread2(result2));
+		}
+
+		public async Task<TOut3> Execute(TIn1 arg)
+		{
+			_arg = arg;
+
+			if (_step1 == null || _step2 == null || _step3 == null)
+			{
+				throw new NullReferenceException("You must provide an IProcessStep in order to execute with supplied argument");
+			}
+
+			return await Execute(_step1, _step2, _step3);
 		}
 	}
 }
